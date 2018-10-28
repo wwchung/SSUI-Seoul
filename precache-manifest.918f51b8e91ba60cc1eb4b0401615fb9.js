f4e4cd8bd884143222a5f67b3fbef4cc215dd351
Won-Woo Chung <wwchung717@gmail.com>
2018-10-27 21:07:59 -0400

    Updates

---
+++ precache-manifest.918f51b8e91ba60cc1eb4b0401615fb9.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "63c47e79488e281baee6d33785484042",
+    "url": "/SSUI-Seoul/static/media/bukchon3.63c47e79.jpg"
+  },
+  {
+    "revision": "1c8fdd31f9ba19bf3fe1",
+    "url": "/SSUI-Seoul/static/css/main.08590936.chunk.css"
+  },
+  {
+    "revision": "03e57a5abaf7cda45ac5",
+    "url": "/SSUI-Seoul/static/js/1.03e57a5a.chunk.js"
+  },
+  {
+    "revision": "03d8c9d507a5099f4c32",
+    "url": "/SSUI-Seoul/static/js/runtime~main.03d8c9d5.js"
+  },
+  {
+    "revision": "89277e34f2c7808131294ca39f312760",
+    "url": "/SSUI-Seoul/static/media/gangnam1.89277e34.jpg"
+  },
+  {
+    "revision": "96631276c49d9942782b94e88f80b599",
+    "url": "/SSUI-Seoul/static/media/gangnam2.96631276.jpg"
+  },
+  {
+    "revision": "91312bef1b7400165bd9063f4fec545f",
+    "url": "/SSUI-Seoul/static/media/gangnam3.91312bef.jpg"
+  },
+  {
+    "revision": "fbaf37e089a90bbeeffad858d3f4047b",
+    "url": "/SSUI-Seoul/static/media/coex1.fbaf37e0.jpg"
+  },
+  {
+    "revision": "42eddf377a4a764e40c91c93ee5f5fa9",
+    "url": "/SSUI-Seoul/static/media/coex2.42eddf37.jpg"
+  },
+  {
+    "revision": "fa7407d8f8286d58579f36fcbc1cf70b",
+    "url": "/SSUI-Seoul/static/media/coex3.fa7407d8.jpg"
+  },
+  {
+    "revision": "1523f12a32abaa841e224c5ee5c17e8f",
+    "url": "/SSUI-Seoul/static/media/bongeunsa1.1523f12a.jpg"
+  },
+  {
+    "revision": "b071e4f5f8087f506e543ebcf1d50a44",
+    "url": "/SSUI-Seoul/static/media/bongeunsa2.b071e4f5.jpg"
+  },
+  {
+    "revision": "edb70b977d091c4de8871f6837c8b46c",
+    "url": "/SSUI-Seoul/static/media/bongeunsa3.edb70b97.jpg"
+  },
+  {
+    "revision": "c25b9b02a2cc6496e48fbf8dd6c241e3",
+    "url": "/SSUI-Seoul/static/media/namsan1.c25b9b02.jpg"
+  },
+  {
+    "revision": "b62eb65eb8fadcafa63d0fa60efa5e12",
+    "url": "/SSUI-Seoul/static/media/namsan2.b62eb65e.jpg"
+  },
+  {
+    "revision": "cae7704da1024442a184fb4e630fd05b",
+    "url": "/SSUI-Seoul/static/media/namsan3.cae7704d.jpg"
+  },
+  {
+    "revision": "9b8a5d694f76a11ff31fecffbe24ff26",
+    "url": "/SSUI-Seoul/static/media/bukchon1.9b8a5d69.jpg"
+  },
+  {
+    "revision": "254f9400fa3f37e0dcf62ec247c52790",
+    "url": "/SSUI-Seoul/static/media/bukchon2.254f9400.jpg"
+  },
+  {
+    "revision": "1c8fdd31f9ba19bf3fe1",
+    "url": "/SSUI-Seoul/static/js/main.1c8fdd31.chunk.js"
+  },
+  {
+    "revision": "285390dbd89c40a5c12f8b9f0bae7d36",
+    "url": "/SSUI-Seoul/static/media/gangnam1.285390db.jpg"
+  },
+  {
+    "revision": "1bae99f384e31f117d21bd8a7e409eda",
+    "url": "/SSUI-Seoul/static/media/gangnam2.1bae99f3.jpg"
+  },
+  {
+    "revision": "3048c05e1e080a15583a97e8b35ffaf7",
+    "url": "/SSUI-Seoul/static/media/gangnam3.3048c05e.jpg"
+  },
+  {
+    "revision": "41c1be714b18a552478fef2d4461315c",
+    "url": "/SSUI-Seoul/static/media/coex1.41c1be71.jpg"
+  },
+  {
+    "revision": "8adbea66011604463109cb884fb75d91",
+    "url": "/SSUI-Seoul/static/media/coex2.8adbea66.jpg"
+  },
+  {
+    "revision": "b8f8862ecd97f3fa98a3aec2d7054815",
+    "url": "/SSUI-Seoul/static/media/coex3.b8f8862e.jpg"
+  },
+  {
+    "revision": "decdfa269ef15172227f7a757d1a65d6",
+    "url": "/SSUI-Seoul/static/media/bongeunsa1.decdfa26.jpg"
+  },
+  {
+    "revision": "89cc2e286b0aace32a62a53f51f5d332",
+    "url": "/SSUI-Seoul/static/media/bongeunsa2.89cc2e28.jpg"
+  },
+  {
+    "revision": "7eeb58b0dc5e292665877d689fb2b41b",
+    "url": "/SSUI-Seoul/static/media/bongeunsa3.7eeb58b0.jpg"
+  },
+  {
+    "revision": "581505f64f0090d292265cfd9c4dbb28",
+    "url": "/SSUI-Seoul/static/media/namsan1.581505f6.jpg"
+  },
+  {
+    "revision": "71f40a88be7edffd2ff5a4189d24837c",
+    "url": "/SSUI-Seoul/static/media/namsan2.71f40a88.jpg"
+  },
+  {
+    "revision": "d2dbbcb2caa09f177bf4d397f2fa7652",
+    "url": "/SSUI-Seoul/static/media/namsan3.d2dbbcb2.jpg"
+  },
+  {
+    "revision": "39d757e754ad4d3cc565204556cbec90",
+    "url": "/SSUI-Seoul/static/media/bukchon1.39d757e7.jpg"
+  },
+  {
+    "revision": "cd26df66bead8e2367c4e12602674248",
+    "url": "/SSUI-Seoul/static/media/bukchon2.cd26df66.jpg"
+  },
+  {
+    "revision": "6a41fd4d7ee42c89e54522e56efdc182",
+    "url": "/SSUI-Seoul/static/media/bukchon3.6a41fd4d.jpg"
+  },
+  {
+    "revision": "39b04404a4d4b330e8dad804e234e2c8",
+    "url": "/SSUI-Seoul/static/media/seoul.39b04404.mp4"
+  },
+  {
+    "revision": "6bb26c09a7cb8fcefad048334085142e",
+    "url": "/SSUI-Seoul/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/SSUI-Seoul/precache-manifest.918f51b8e91ba60cc1eb4b0401615fb9.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/SSUI-Seoul/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
